d082f37c64cf95c77700dbc6ccc101b42e17e557
Ross10 <itai.russo@yahoo.com>
2017-01-14 13:47:54 +0200

    login design  +  added some photos

--- app/src/main/java/com/example/ross/baby_saver/Login_Activity.java
+++ app/src/main/java/com/example/ross/baby_saver/Login_Activity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login_Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_);
+       setContentView(R.layout.activity_login_);
     }
 }
