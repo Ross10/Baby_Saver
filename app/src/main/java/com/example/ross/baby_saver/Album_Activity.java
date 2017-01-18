6a2866bd83a6409901a8aa4154b1f2828519ece9
neviko <nevosayag@gmail.com>
2017-01-18 02:22:32 +0200

    Album functionalty done

--- app/src/main/java/com/example/ross/baby_saver/Album_Activity.java
+++ app/src/main/java/com/example/ross/baby_saver/Album_Activity.java
@@ -3,8 +3,10 @@ package com.example.ross.baby_saver;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -12,8 +14,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.Time;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,6 +31,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -31,80 +41,88 @@ import java.util.TimeZone;
  */
 
 
-public class Album_Activity extends AppCompatActivity
-{
+public class Album_Activity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_ACTIVITY_CALLBACK = 2;
     private Bitmap imageBitmap;
     private ImageView cameraIV;
     private File directory;
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+    private String path;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
         //TODO load the album from the internal storage
-        cameraIV = (ImageView)findViewById(R.id.cameraImageView);
+        cameraIV = (ImageView) findViewById(R.id.cameraImageView);
 
 
         //testing area
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         // path to /data/data/yourapp/app_data/imageDir
         directory = cw.getDir("BabySaver", Context.MODE_PRIVATE);
-        String path = directory.getPath();
-        loadImageFromStorage(path);
+        path = directory.getPath();
 
-        ////////////////////////////////////////////////////////////
-//        File mydir = this.getDir("BabySaver", Context.MODE_PRIVATE); //Creating an internal dir;
-//        if (!mydir.exists())
-//        {
-//            mydir.mkdirs();
-//        }
+
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+
+
+        //event listener for image click
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+                //Create intent
+                Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("image", item.getImage());
+
+                //Start details activity
+                startActivity(intent);
+            }
+        });
     }
 
 
-
-    public void openCamera(View v)
-    {
+    public void openCamera(View v) {
         Intent takePicIntante = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // create a new intent for cameraIV transfer between activities.
-        if(takePicIntante.resolveActivity(getPackageManager()) != null)
-            startActivityForResult(takePicIntante,REQUEST_IMAGE_CAPTURE);// callback function
+        if (takePicIntante.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takePicIntante, REQUEST_IMAGE_CAPTURE);// callback function
     }
 
 
     //this function will call right after the startActivityForResult and it will happend after the camera will finish her job and want to transfer the data to our activity.
     protected void onActivityResult(int requestCode, int resultCode, Intent data) // override the result function must implement with startActivityForResult
     {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap)extras.get("data");
-            //cameraIV.setImageBitmap(imageBitmap);
-            //TODO Here i need to save images into the local storage
-            // TODO refresh the album and redisplayed it.
+            imageBitmap = (Bitmap) extras.get("data");
 
 
-            //saveToInternalStorage(imageBitmap);
+            saveToInternalStorage(imageBitmap);
             String path = directory.getPath();
+            gridAdapter.notifyDataSetChanged();
 
-            loadImageFromStorage(path);
 
 
-        }
-        else if(requestCode==REQUEST_ACTIVITY_CALLBACK){
+        } else if (requestCode == REQUEST_ACTIVITY_CALLBACK) {
 
-            Toast.makeText(this, "No image was captured, Try again", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "קרתה תקלה בעת הצילום, אנא נסה שנית", Toast.LENGTH_SHORT).show();
         }
 
 
     }
 
 
-
-
-
-    private String saveToInternalStorage(Bitmap bitmapImage){
+    private String saveToInternalStorage(Bitmap bitmapImage) {
 
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"));
         Date currentLocalTime = cal.getTime();
@@ -117,9 +135,9 @@ public class Album_Activity extends AppCompatActivity
 
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         // path to /data/data/yourapp/app_data/imageDir
-        directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        directory = cw.getDir("BabySaver", Context.MODE_PRIVATE);
         // Create imageDir
-        File mypath=new File(directory,imageID);
+        File mypath = new File(directory, imageID);
 
         FileOutputStream fos = null;
         try {
@@ -142,34 +160,33 @@ public class Album_Activity extends AppCompatActivity
 
 
 
-
-
-    private void loadImageFromStorage(String path)
+    private ArrayList<ImageItem> getData()
     {
 
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
 
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
 
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
-            }
-        }
-
-
-        try {
-            File f=new File(path, "profile.jpg");
-            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
-            ImageView img=(ImageView)findViewById(R.id.iv1);
-            img.setImageBitmap(b);
-        }
-        catch (FileNotFoundException e)
+        for (int i = 0; i < listOfFiles.length; i++)
         {
-            e.printStackTrace();
+            File f = new File(listOfFiles[i].toString());
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(new FileInputStream(f));
+                imageItems.add(new ImageItem(bitmap, listOfFiles[i].getName()));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
         }
 
+
+        return imageItems;
     }
+
+
 
 
 }
